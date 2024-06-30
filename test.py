1e991b42a5f32b4f6f111c3a77927d749a8eb9df
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-06-30 11:51:52 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr)
+print(arr.ndim)
+
+# 5x5 size of the array and the data is 10
+n1 = np.full((5,5),10)
+print(n1)
+
+# print in range (start from 0 to n-1)
+n2 = np.arange(50,100)
+print(n2)
